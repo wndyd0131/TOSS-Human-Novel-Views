aeabd0e0a02c723c5e73730f864bd9ae23103cd6
wndyd <wndyd0131@naver.com>
2026-06-24 07:01:24 -0500

    oracle test

--- cldm/toss_lora.py
+++ cldm/toss_lora.py
@@ -646,8 +646,12 @@ class TossLoraModule(TOSS):
             refine_sub = sel_refine[sel_decode]
             if torch.any(refine_sub):
                 n_coarse = self.normal_head(dec_feat[sel_refine])
-                pred_rgb_ref = pred_rgb[refine_sub]
-                n_refined = self.normal_refine_head(pred_rgb_ref, n_coarse)
+
+                # oracle test: use GT target RGB instead of decoded pred_rgb
+                gt_rgb_all = self._gt_rgb_01_from_batch(batch)
+                gt_rgb_ref = gt_rgb_all[sel_refine]
+
+                n_refined = self.normal_refine_head(gt_rgb_ref, n_coarse)
 
                 gt_normals, normal_mask = self._prepare_normal_gt(
                     batch, sel_refine, n_refined.shape[-2:]
